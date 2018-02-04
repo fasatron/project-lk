7d8d133796f0c7eeffa710a4c35f58dd894ce9f3
Ruslan <ruslfm08@gmail.com>
2018-02-04 18:04:57 +0500

    change User model

--- models/User.js
+++ models/User.js
@@ -1,18 +1,29 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
-const UserSchema = new Schema(
+const userSchema = new Schema(
   {
-    _id: Schema.Types.ObjectId,
     first_name: { type: String, default: '', trim: true, required: true },
     last_name: { type: String, default: '', trim: true, required: true },
-    email: { type: String, default: '', trim: true, required: true },
+    email: {
+      type: String,
+      default: '',
+      trim: true,
+      required: true,
+      unique: true,
+      match: [
+        /^[a-zA-Z0-9'._%+-]+@[a-zA-Z0-9-][a-zA-Z0-9.-]*\.[a-zA-Z]{2,63}$/,
+        'Incorrect email',
+      ],
+    },
+    password: { type: String, required: true, min: 7 },
     image: { data: Buffer, contentType: String },
     avatar: {
       type: String,
       default: 'http://armanpalace.ru/css/images/noava.svg',
     },
-    about: { type: String, default: '', required: true },
+    about: { type: String, default: '', trim: true },
     rating: { type: Number, min: 0, max: 5, default: 0 },
     rate: { type: Number, min: 0, default: 0 },
     skills: [{ type: String, ref: 'Skill' }],
@@ -21,15 +32,53 @@ const UserSchema = new Schema(
     toObject: { getters: false, virtuals: false },
     toJSON: { versionKey: false, getters: true },
     timestamps: true,
-  }
+  },
 );
 
-UserSchema.virtual('profile_url').get(function() {
-  return '/users/' + this._id;
+userSchema.pre('save', function(next) {
+  if (!this.isModified('password')) return next();
+
+  bcrypt
+    .hash(this.password, 10)
+    .then(hash => {
+      this.password = hash;
+      next();
+    })
+    .catch(next);
 });
 
-UserSchema.virtual('full_name').get(function() {
+userSchema.post('save', function(error, user, next) {
+  if (error.name === 'MongoError' && error.code === 11000) {
+    next(new Error('User with the same email already exists'));
+  } else {
+    next(error);
+  }
+});
+
+userSchema.statics.authenticate = function(email, password) {
+  return this.findOne({ email }).then(user => {
+    if (!user) {
+      const error = new Error('User not found');
+
+      error.status = 401;
+      throw error;
+    }
+
+    return bcrypt.compare(password, user.password).then(isEqual => {
+      if (!isEqual) {
+        const error = new Error('Incorrect password');
+
+        error.status = 401;
+        throw error;
+      }
+
+      return user;
+    });
+  });
+};
+
+userSchema.virtual('full_name').get(function() {
   return `${this.first_name} ${this.last_name}`;
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', userSchema);
