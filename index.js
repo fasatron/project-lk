17a67af83332693909b3ddd3294927087e18b39a
Ruslan <ruslfm08@gmail.com>
2018-02-04 17:56:07 +0500

    use session with express-session and connect-mongodb-session

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const path = require('path');
 const express = require('express');
-const robots = require('express-robots')
-const favicon = require('serve-favicon')
+const robots = require('express-robots');
+const favicon = require('serve-favicon');
 const logger = require('morgan');
+const session = require('express-session');
+const MongoStore = require('connect-mongodb-session')(session);
 
-const connection = require('./services/db');
+const db = require('./services/db');
 const admin = require('./admin');
-const routers = require('./routers')
-const { error } = require('./middleware');
+const routers = require('./routers');
+const { error, auth } = require('./middleware');
 const config = require('./config');
 
-const { paths, port } = config
+const { paths, port } = config;
 const server = express();
 
 server.set('view engine', 'pug');
@@ -20,18 +22,40 @@ server.locals.basedir = paths.views;
 
 server.use(express.static(paths.public));
 server.use(favicon(path.join(paths.public, 'favicon.ico')));
-server.use(robots({UserAgent: '*', Disallow: '/'}));
+server.use(robots({ UserAgent: '*', Disallow: '/' }));
 server.use(express.urlencoded({ extended: false }));
-
+server.use(
+  session({
+    name: 'sessionId',
+    secret: config.sessionSecret,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      // secure: true,
+      signed: true,
+      maxAge: 1000 * 60 * 60 * 24 * 3, // 3 days
+    },
+    store: new MongoStore({
+      mongooseConnection: db.connection,
+      touchAfter: 60 * 60 * 24, // 1 day
+    }),
+  }),
+);
 server.use(logger('dev'));
+server.use(auth.findUser);
 
 server.use('/', routers.main);
-server.use('/users', routers.user);
 server.use('/auth', routers.auth);
+server.use('/users', routers.user);
+
+server.use(auth.authenticated);
+server.use('/profile', routers.profile);
 server.use('/admin', admin);
 
 server.use(error.notFound);
-server.use(server.get('env') === 'development' ? error.development : error.production);
-
+server.use(
+  server.get('env') === 'development' ? error.development : error.production,
+);
 
 server.listen(port, () => console.info('Server running on port', port));
