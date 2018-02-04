8eceba2309b0040fa7e2479a96d6864409a6e206
Ruslan <ruslfm08@gmail.com>
2018-02-04 18:05:40 +0500

    change autj router

--- routers/auth.js
+++ routers/auth.js
@@ -1,8 +1,35 @@
 const router = require('express').Router();
 
-const { auth: { showRegisterPage, showLoginPage } } = require('../controllers');
+const {
+  validation: {
+    email,
+    password,
+    confirmPassword,
+    validationResult,
+    errorFormatter,
+  },
+} = require('../services/validation');
 
-router.get('/register', showRegisterPage);
-router.get('/login', showLoginPage);
+const {
+  auth: { showRegisterPage, showLoginPage, login, register, logout },
+} = require('../controllers');
+const {
+  auth: { authenticated, unauthenticated },
+  validation: { validate },
+} = require('../middleware');
+
+router
+  .route('/register')
+  .all(unauthenticated)
+  .get(showRegisterPage)
+  .post([email, password, confirmPassword], validate, register);
+
+router
+  .route('/login')
+  .all(unauthenticated)
+  .get(showLoginPage)
+  .post([email, password], validate, login);
+
+router.get('/logout', authenticated, logout);
 
 module.exports = router;
