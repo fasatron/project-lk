22eccaf8ae68933608ba195e205e6bbd21951d31
Ruslan <ruslfm08@gmail.com>
2018-02-07 23:07:58 +0500

    configure oauth providers

--- config/index.js
+++ config/index.js
@@ -12,4 +12,23 @@ module.exports = {
     lib: path.resolve(__dirname, '..', 'node_modules'),
   },
   mongodbUrl: process.env.MONGODB_URL,
+  oauth: {
+    github: {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.GITHUB_CALLBACK_URL,
+    },
+    facebook: {
+      clientID: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+      profileFields: ['id', 'displayName', 'photos', 'email'],
+    },
+    vk: {
+      clientID: process.env.VK_CLIENT_ID,
+      clientSecret: process.env.VK_CLIENT_SECRET,
+      callbackURL: process.env.VK_CALLBACK_URL,
+      profileFields: ['email', 'friends'],
+    },
+  },
 };
